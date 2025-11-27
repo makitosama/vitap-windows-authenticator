d1f3f7535ef7595bb66f8579abaa29f161bcd2d0
makitosama <238057123+makitosama@users.noreply.github.com>
2025-11-27 17:32:11 +0530

    Fix config - Correct host to 172.18.10.10 and port to 8090 for actual VIT-AP portal

--- VitapClient.cs
+++ VitapClient.cs
@@ -11,10 +11,8 @@ namespace VitapAuthenticator
     public class VitapClient
     {
         // Configuration constants
-        private const string VITAP_HOST = "172.18.10.15";
-        private const int VITAP_PORT = 8080;
-        private static readonly string VITAP_BASE_URL = "http://" + VITAP_HOST + ":" + VITAP_PORT;
-        private static readonly RestClientOptions options = new RestClientOptions(VITAP_BASE_URL)
+private const string VITAP_HOST = "172.18.10.10";        private const int VITAP_PORT = 8080;
+private const int VITAP_PORT = 8090;        private static readonly RestClientOptions options = new RestClientOptions(VITAP_BASE_URL)
         {
             FollowRedirects = true,
             MaxRedirects = 5,
